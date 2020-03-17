c1fd1ff99d23ed539777d70ed4e11121d5748652
LeeChangSu <lcscs6152@gmail.com>
2020-03-17 19:08:32 +0900

    Updates

---
+++ precache-manifest.ee07d9ffb2e29636f1c92a6d23b72066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22efe3b82aefb24ee69f0a98c6e9f37f",
+    "url": "/lcs6152/movie_app/index.html"
+  },
+  {
+    "revision": "17d918b4488db7b43e23",
+    "url": "/lcs6152/movie_app/static/css/main.731b0bf4.chunk.css"
+  },
+  {
+    "revision": "dc94e4081f918af4367e",
+    "url": "/lcs6152/movie_app/static/js/2.3aea1428.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/lcs6152/movie_app/static/js/2.3aea1428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17d918b4488db7b43e23",
+    "url": "/lcs6152/movie_app/static/js/main.2faf4bb6.chunk.js"
+  },
+  {
+    "revision": "ec69b2ef5118fba055f2",
+    "url": "/lcs6152/movie_app/static/js/runtime-main.0c8244d5.js"
+  }
+]);
